e65c85b85d61fd427297847ca9bc60caf4c775af
Ilya Kozhemyakin <kozhilya@gmail.com>
2025-09-02 18:26:24 +0400

    remove uned file that causing windows to error

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-// This file is no longer needed as we're using React components
-// The application is now managed by React components in the components folder
-
-import './styles/main.scss';
-import { LanguageManager } from './script/managers/language-manager';
-
-// Initialize language manager
-LanguageManager.initialize();
