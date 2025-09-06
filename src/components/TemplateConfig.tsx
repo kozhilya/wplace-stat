7f1cd4f1240a13fff9c752dd2378e8941a66db8e
Ilya Kozhemyakin <kozhilya@gmail.com>
2025-09-06 18:15:14 +0400

    feat: add TemplateSaveEventArts import to TemplateConfig

--- src/components/TemplateConfig.tsx
+++ src/components/TemplateConfig.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Template } from '../types/template';
 import { debug } from '../utils';
 import { LanguageManager } from '../managers/language-manager';
-import { LanguageChangeEventArts } from '../types/event-args';
+import { LanguageChangeEventArts, TemplateSaveEventArts } from '../types/event-args';
 import { EventManager } from '../managers/event-manager';
 
 interface TemplateConfigProps {
